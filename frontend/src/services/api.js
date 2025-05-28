7208198652614c4b8420730196ea239439f4a72d
NakVong <kiriratanakvong@berkeley.edu>
2025-05-28 13:51:44 -0700

    added API calls

---
+++ frontend/src/services/api.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const res = await fetch(`${BASE_URL}/movie/popular?api_key=${process.env.API_KEY}`)
+    const data = await res.json()
+    return data.results
+}
+
+export const searchMovies = async (query) => {
+    const res = await fetch(`${BASE_URL}/search/movie?api_key=${process.env.API_KEY}&query=${encodeURIComponent(query)}`)
+    const data = await res.json()
+    return data.results
+}
