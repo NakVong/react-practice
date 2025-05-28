e4b15c5c4abb5ba4b220b75281ab49ed3de220a5
NakVong <kiriratanakvong@berkeley.edu>
2025-05-27 22:46:42 -0700

    setup with movie-card component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MovieCard from './components/MovieCard'
+
+function App() {
+  return (
+    <>
+      <MovieCard movie={{title: "Nak's Film", release_date: "2024"}}></MovieCard>
+    </>
+  )
+}
+
+export default App
