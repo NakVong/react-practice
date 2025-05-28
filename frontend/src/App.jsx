b6c36ab14ae25a370cc1f44daaf84e576611daf9
NakVong <kiriratanakvong@berkeley.edu>
2025-05-27 23:04:10 -0700

    added a homepage with dynamic rendering of movies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MovieCard from './components/MovieCard'
+import Home from './pages/Home'
 
 function App() {
-  const movieNumber = 2
-
   return (
     <>
-      {movieNumber === 1 ? 
-        <MovieCard movie={{title: "Nak's Film", release_date: "2024"}}></MovieCard> : 
-        <MovieCard movie={{title: "Not Nak's Film", release_date: "2024"}}></MovieCard>
-      }
+      <Home></Home>
     </>
   )
 }
