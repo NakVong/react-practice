1159891f3636e5fcc9bf9c7b27b348591e11b6f4
NakVong <kiriratanakvong@berkeley.edu>
2025-05-28 14:11:28 -0700

    removed unnecessary dependency

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 dotenv.config()
 const BASE_URL = "https://api.themoviedb.org/3"
 
+console.log(process.env.API_KEY)
+
 export const getPopularMovies = async () => {
     const res = await fetch(`${BASE_URL}/movie/popular?api_key=${process.env.API_KEY}`)
     const data = await res.json()
